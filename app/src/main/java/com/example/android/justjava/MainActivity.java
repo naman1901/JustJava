1bdb8bd0c79cc824155c1ba72565021fccaef8f9
naman1901 <naman1901@gmail.com>
2015-07-17 20:16:44 +0530

    Commit 2

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.justjava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
